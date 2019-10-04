979fc2a98a03361be4e832eeebce2ed5e311ec33
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-03 21:22:22 -0400

    did 1.2.7

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,21 +3,28 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-
+var firstDecimal = 1.1
+var secondDecimal = 2
 
 /*:
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
+var trueOrFalse = true
+//firstDecimal = trueOrFalse
+// it can not be compiled because trueOrFalse is a boolean and firstDecimal is a variable
 
 
 /*:
  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
-
+var StringValue = "Idk what to write here"
+//firstDecimal = StringValue
+// The same as with the boolean, this is a different type of a variable, can not compile
 /*:
  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
+//firstDecimal = secondDecimal
+// this will not compile because one it an integer and the other one is a double. Even though they are both numbers, they are different kinds of variables.
 
 
 //: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
