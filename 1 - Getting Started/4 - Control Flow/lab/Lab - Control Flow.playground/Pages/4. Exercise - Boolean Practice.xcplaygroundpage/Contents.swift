f7b65f16335fdb42d2efe0e52082e891c790ee36
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-05 21:49:54 -0400

    did 1.4.4

--- 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
@@ -12,14 +12,28 @@
 let hasFish = true
 let hasPizza = false
 let hasVegan = true
-
-
+// if both of these statements are true the output will be Let's go
+if hasFish && hasVegan == true {
+    print("Let's Go!")
+} else if hasPizza && hasVegan == true {
+    print("Letls go")
+} else {
+    print("Sorry, we will have to think of somewhere else.")
+}
 /*:
  Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and sunny out. Create a constant `isNiceWeather` that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice.
  */
 let temp = 82
-let isRaining = true
-let isSunny = true
+// has to be a variable
+var isRaining = false
+var isSunny = true
+// if all of those 3 statements are true then isNiceWeather will = true 
+var isNiceWeather = (isSunny == true) && (temp >= 82) && (isRaining == false)
 
+if isNiceWeather == true {
+ print("I'm going for a walk!")
+} else {
+    print("I'm going to stay at my house!")
+}
 
 //: [Previous](@previous)  |  page 4 of 9  |  [Next: App Exercise - Target Heart Rate](@next)
