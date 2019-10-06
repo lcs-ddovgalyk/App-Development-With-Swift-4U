a943624c053c08b63296986979a85ee4bb6da6f7
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-05 21:25:21 -0400

    finished 1.3 finished 1.4.1

--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/8. App Exercise - Fitness Conversions.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/8. App Exercise - Fitness Conversions.xcplaygroundpage/Contents.swift
@@ -7,7 +7,9 @@
 
  Now create a constant `percentOfGoal` of type `Double` that equals the percent of the goal that has been reached so far. You'll need to convert your constants of type `Int` to be of type `Double` in your calculation.
  */
-
+let steps = 500.0
+let goal = 10000.0
+let percentOfGoal = steps/goal
 
 /*:
 
--- 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
@@ -10,46 +10,105 @@
  
  1. `9 == 9`
  */
-
+//defins the first variable
+let x = 9
+//defins the equation
+//if x is equal to 9 the exression will be true
+if x == 9 {
+print("true")
+// if x is not equal to 9 the expression will be false
+} else {
+print("false")
+}
 
 /*:
  2. `9 != 9`
  */
 
+let x2 = 9
+if x2 != 9 {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 /*: 
  3. `47 > 90`
  */
-
+let x3 = 47
+if x3 > 90 {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 /*:
  4. `47 < 90`
  */
-
+let x4 = 47
+if x4 < 90 {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 /*:
  5. `4 <= 4`
  */
-
+let x5 = 4
+if x5 <= 4 {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 /*:
  6. `4 >= 5`
  */
-
+let x6 = 3
+if x6 >= 4 {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 /*:
  7. `(47 > 90) && (47 < 90)`
  */
-
+let x7 = (47>90)
+if x7 && (47<90) {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 /*:
  8. `(47 > 90) || (47 < 90)`
  */
+let x8 = (47>90)
+if x8 || (47<90) {
+    print("true")
+    
+} else {
+    print("false")
+}
 
 
 /*:
  9. `!true`
  */
+let x9 = true
+if x9 == !true {
+    print("true")
+} else {
+    print("false")
+}
 
 
 //: page 1 of 9  |  [Next: Exercise - If and If-Else Statements](@next)
