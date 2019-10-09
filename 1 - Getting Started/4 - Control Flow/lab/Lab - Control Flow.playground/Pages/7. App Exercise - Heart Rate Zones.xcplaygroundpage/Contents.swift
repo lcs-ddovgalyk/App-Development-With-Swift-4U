a857d9c9c303f8e33b6ca0359f2363cfd1f4e9a4
lcs-ddovgalyk <ddovgalyk@lcs.on.ca>
2019-10-08 21:38:40 -0400

    did 1.4.7

--- 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/7. App Exercise - Heart Rate Zones.xcplaygroundpage/Contents.swift
@@ -15,7 +15,41 @@
  
  If `currentHR` is above the listed zones, print some kind of warning asking the user to slow down.
  */
+// defines different stages at different hearts rates
+//current heart rate of the person
+
 let currentHR = 128
-
-
+let recoveryHR = 100
+let lightZone = 121
+let moderateZone = 141
+let hardZone = 161
+let maximumZone = 181
+let warningZone = 201
+//recovery heart rate
+   // if the current heart rate is in between the recovery heart rate zone
+   // and light heart zone it will print the following message
+// this works for every following if or else-if statements
+    //bigger or equals to  //and        //less than
+if currentHR >= recoveryHR && currentHR < lightZone{
+    // it will print this message
+    print("You are in the Very Light zone. Activity in this zone helps with recovery")
+//light zone
+//this is the moderate zone so it will print the light zone message
+} else if currentHR >= lightZone && currentHR < moderateZone {
+    print("You are in the Light zone. Activity in this zone helps improve basice endurance and fat burning.")
+//moderate zone
+} else if currentHR >= moderateZone && currentHR < hardZone {
+    print("You are in the Moderate zone. Activity in this zone helps improve aerobic fitness.")
+//hard zone
+} else if currentHR >= hardZone && currentHR < maximumZone {
+    print("You are in the Hard zone. Activity in this zone increases maximum performance capacity for shorter sessions.")
+//maximum zone
+} else if currentHR >= maximumZone && currentHR < warningZone {
+    print("You are in the Maximum zone. Activity in this zone helps fit athletes develop speed.")
+//warning zone
+// this does not need a '&&' ( and ) because if it equals to the warning zone heart limit or IS higher than the warning heart zone than it will print a warning messgage this does not need to have a limit
+} else if currentHR >= warningZone {
+    print("Warning! Slow down! You're heart rate is very high! This is not healthy for your body")
+    
+}
 //: [Previous](@previous)  |  page 7 of 9  |  [Next: Exercise - Ternary Operator](@next)
